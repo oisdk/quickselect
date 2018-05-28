25ee1d6c7babc444f97c7998e0ea57991d5b7500
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-05-28 01:29:45 -0400

    conditional compilation older GHCs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
 
 import           Test.QuickCheck
@@ -15,6 +17,10 @@ main =
     do NonEmpty xs <- arbitrary :: Gen (NonEmptyList Int)
        i <- choose (0, length xs - 1)
        let ys = sort xs
+#if MIN_VERSION_base(4,7,0)
        pure $
+#else
+       return $
+#endif
            (ys !! i) === select i (Vector.fromList xs) .&&. (ys !! i) ===
            U.select i (UVector.fromList xs)
