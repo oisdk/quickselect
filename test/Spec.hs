7b341fbb5bacc3a35dcc36d4ab990c72598fe920
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-05-28 00:56:09 -0400

    hedgehog -> quickcheck

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Main (main) where
 
-import           Hedgehog
-import qualified Hedgehog.Gen               as Gen
-import qualified Hedgehog.Range             as Range
+import           Test.QuickCheck
 
 import           Data.List                  (sort)
 import qualified Data.Vector                as Vector
@@ -13,19 +9,12 @@ import qualified Data.Vector.Unboxed        as UVector
 import           Data.Select.Vector
 import qualified Data.Select.Vector.Unboxed as U
 
-prop_median :: Property
-prop_median =
-    property $
-    do xs <-
-           forAll
-               (Gen.list
-                    (Range.linear 1 100)
-                    (Gen.int (Range.linear 0 100)))
-       let l = length xs
-       i <- forAll (Gen.int (Range.linear 0 (l-1)))
+main :: IO ()
+main =
+    quickCheck $
+    do NonEmpty xs <- arbitrary :: Gen (NonEmptyList Int)
+       i <- choose (0, length xs - 1)
        let ys = sort xs
-       (ys !! i) === select i (Vector.fromList ys)
-       (ys !! i) === U.select i (UVector.fromList ys)
-
-main :: IO Bool
-main = checkParallel $$(discover)
+       pure $
+           (ys !! i) === select i (Vector.fromList xs) .&&. (ys !! i) ===
+           U.select i (UVector.fromList xs)
